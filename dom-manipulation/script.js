f1533d29060cfa25ffb9ba4a6fb806b80ae03f41
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-23 19:19:22 +0300

    added on click functionality

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,31 +1,30 @@
 let quotes = [
-  { text: "So many books, so little time.", 
-    category: "Humor" },
+  { text: "So many books, so little time.", category: "Humor" },
   {
     text: "A room without books is like a body without a soul.",
-    category: "Philosophy"
+    category: "Philosophy",
   },
   {
     text: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
-    category: "Love"
+    category: "Love",
   },
   {
-    text:"I Think therefore I am",
-    category: "Philosophy"
-  }
+    text: "I Think therefore I am",
+    category: "Philosophy",
+  },
 ];
 //show random Quote fun
-function showRandomQuto(){
-    //math.floor-rounds down 
-    //math.random() gives a random number between 0 and 1
-    var randomIndex =Math.floor(Math.random()*quotes.length);
-    var randomQuote = quotes[randomIndex];
-
+function showRandomQuote() {
+  //math.floor-rounds down
+  //math.random() gives a random number between 0 and 1
+  var randomIndex = Math.floor(Math.random() * quotes.length);
+  var randomQuote = quotes[randomIndex];
+  document.getElementById("newQuote").innerText = randomQuote.text;
 }
 
 //create random Quote fun
-function createAddQuoteForm(){
-    const form=`
+function createAddQuoteForm() {
+  const form = `
    
     <div>
         <input id="newQuoteText" type="text" placeholder="Enter a new quote"/>
@@ -33,6 +32,5 @@ function createAddQuoteForm(){
         <button onclick="addQuote()">Add Quote</button>
 
     </div>`;
-    document.getElementById('quoteForm').innerHTML=form;
+  document.getElementById("quoteForm").innerHTML = form;
 }
-
