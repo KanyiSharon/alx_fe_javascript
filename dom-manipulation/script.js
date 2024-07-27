86c3aaaf9622de5e3e28122be2ebec80f3bfa299
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-27 15:00:01 +0300

    made changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -26,7 +26,7 @@ function saveQuotes() {
 }
 
 // Populate category filter dropdown
-function populateCategoryFilter() {
+function CategoryFilter() {
   const categories = Array.from(new Set(quotes.map((quote) => quote.category)));
   const categoryFilter = document.getElementById("categoryFilter");
 
