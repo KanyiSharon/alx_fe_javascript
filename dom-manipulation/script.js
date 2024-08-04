e5a0ccca1afec4f6e03348fb1c330a362a89bff4
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-04 16:14:59 +0300

    made changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -66,7 +66,7 @@ async function syncQuotes() {
   saveQuotes();
   populateCategories();
   filterQuotes();
-  notifyUser("Quotes synced with server");
+  notifyUser("Quotes synced with server!");
 }
 
 // Resolve conflicts by giving precedence to server data
